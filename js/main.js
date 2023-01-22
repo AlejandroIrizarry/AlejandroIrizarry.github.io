c6ba57c73c7b6d17de73daa3a121fe1d185ce30b
Alejandro Irizarry <alejandro.irizarry3@upr.edu>
2023-01-22 17:49:43 -0400

    New site components/media.

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementsByClassName('toggle-button')[0]
-const navLinks = document.getElementsByClassName('nav-links')[0]
-
-toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
-})
